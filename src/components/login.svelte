c111dbfff8d519c975a1c8280cc37c85cdd0c2e7
Charles Harries <charles@harries.me>
2022-03-25 15:44:08 +0000

    Fall back to Vercel URL if available

--- src/components/login.svelte
+++ src/components/login.svelte
@@ -1,7 +1,12 @@
 <script>
 	const clientID = import.meta.env.VITE_CLIENT_ID;
 	const appURL = import.meta.env.VITE_APP_URL;
+	const vercelURL = import.meta.env.VERCEL_URL;
 	let url = `http://www.strava.com/oauth/authorize?client_id=${clientID}&response_type=code&redirect_uri=${appURL}&approval_prompt=force&scope=read`;
+
+	if (vercelURL) {
+		url = `http://${vercelURL}`;
+	}
 </script>
 
 <a href={url}>Log in</a>
