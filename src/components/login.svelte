194922713e4ed0cad6a92123de384373aaf8cf5b
Charles Harries <charles@harries.me>
2022-03-26 12:29:33 +0000

    Load basic polyline into geojson into map

--- src/components/login.svelte
+++ src/components/login.svelte
@@ -2,7 +2,7 @@
 	const clientID = import.meta.env.VITE_CLIENT_ID;
 	const appURL = import.meta.env.VITE_APP_URL;
 	const vercelURL = import.meta.env.VERCEL_URL;
-	let url = `http://www.strava.com/oauth/authorize?client_id=${clientID}&response_type=code&redirect_uri=${appURL}&approval_prompt=force&scope=read`;
+	let url = `http://www.strava.com/oauth/authorize?client_id=${clientID}&response_type=code&redirect_uri=${appURL}&approval_prompt=force&scope=read,activity:read_all`;
 
 	if (vercelURL) {
 		url = `http://${vercelURL}`;
--- src/components/map.svelte
+++ src/components/map.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
 	import 'mapbox-gl/dist/mapbox-gl.css';
 	import mapboxgl from 'mapbox-gl';
+	import polyline from '@mapbox/polyline';
 	import { onMount } from 'svelte';
 
 	let container: HTMLElement;
 	let map: mapboxgl.Map;
+	let line =
+		'm~wiIherMjFpUtBtFE`@yOwDyLqHkA`CuDpCyFpB_C~CoCpBcShEgElPe@o@wADgGlGkBB{BrBsHfCg@~@kCD}C^k@`A}AKSjA_BtAgA^iAlDsBb@aEtFcEpB_AiBHo@uEgDuBy@Oo@_AV}^_R_D[i\\eLkLMmIkB_CmCiE_BuIuGIeB}@kFeDeO{@gMaAwD\\q^WkBOiQk@wElBwGpA{JjDwEdAcFd@_IhToP~@eClDcH`GuAhFKf@yBr@a@hD{@jA`@Tu@`EfDfGX|BxAvDbFlNvFhDxExCdCtL`CpM_@tFkAlAqAhF{BpCeHnD{CbEmKlC}DfBcJvIiN\\Ve@gD}BuHGuB\\}J`D}L|@mHjJkI~AgGpEmDnCcHzDaOfK}O|IsCK_I|@{InIsItB}@`CoDCq@]MmGt@mB_EsAcHLaDSsBx@gDEiCxBkFmAeLRmEo@sV`AwCrCiPNsDa@q@G}At@oA|Aw@z@{Dk@{HrHgKHkJT_An@CtFiEdC{CzByA`AkDFcChCuJHwKlEk@h@eBbDoD|Ac@pImLrHiOhAoEvB_BrMmPlCrA|M`Bf_@yBtKtAhFaBUu@c@eQ\\cBj@sTn@oFrAqAfDUtCaFlAJlBmHJuI`CyAjAgCxBmQlAwRZiA|DaFzBiPZ_L\\wAq@yCNm_@^qBiA_IYuJq@kKsA_MgA}@De@vG_CtFLhCb@bD|BnDhAnHyJpD_ASFRg@nItKjEzATe@Hf@`Ab@d@oAtAUdAhBjBnBfCz@YvPbAzAu@dBQhBNtVxC|Hb@lEdAf]`A|G\\xKdCp\\jInWvDhGIlBsAz@cA`D{@nEAzGz@jEj@fOtCxDJxBtAvAe@nAb@nGUp@iLlFwGbGsAnBPV[MRj@Km@WHp@HaAWTMXNWUsAnCpBbPLjD_IvDeLxQYlAD`Ad@bFfArDtGd@b@nBGbAq@bEyEhP}@fSmC`Ch@nFwA|DNhBeAnAmBjHClAzAfIaAlIObL_AdMw@jE@zBkBjQCbDc@~@SxEaAnFmAhUoBrN]`J{CpXuC|MNhJW`J^lDcAzEqDpB{AYaAjAuBtFUdEd@lIOvK_CbHJRQIaErN}@zLFrC]dEPlGkJpV}Jhe@eApV@rFo@jGRFQdCVp@U`AZrFYhDg@dALrDbAzAPbNnA`JQ?e@eHg@yMcDyNg@gEiByG{B_@s@aASpAyGbEeEfFkH|FuKl@}FdDeHx@mEnFgCzA{BaBqDnBqFCsB|@oFnFuDcEyE{A}C{CwDiB_AqBqCsBgEiAeNdAsIqCi@h@?bB[\\cBuF';
 
 	onMount(async () => {
 		if (typeof import.meta.env.VITE_MAPBOX_TOKEN === 'string') {
@@ -16,10 +19,34 @@
 		map = new mapboxgl.Map({
 			container: container,
 			style: 'mapbox://styles/mapbox/dark-v10',
-			center: [-74.5, 40],
-			zoom: 9
+			center: [-2.0730246, 54.9778726],
+			zoom: 7
+		});
+
+		map.on('load', () => {
+			map.addSource('route', {
+				type: 'geojson',
+				data: {
+					type: 'Feature',
+					properties: {},
+					geometry: polyline.toGeoJSON(line)
+				}
+			});
+			map.addLayer({
+				id: 'route',
+				type: 'line',
+				source: 'route',
+				layout: {
+					'line-join': 'round',
+					'line-cap': 'round'
+				},
+				paint: {
+					'line-color': '#bada55',
+					'line-width': 3
+				}
+			});
 		});
 	});
 </script>
 
-<div bind:this={container} style="height: 400px;" />
+<div bind:this={container} style="height: 500px;" />
