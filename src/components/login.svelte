9a85c40a8439f718634191b52215704d387297b7
Charles Harries <charles@harries.me>
2022-03-26 14:42:10 +0000

    Couple more tweaks to redirect url

--- src/components/login.svelte
+++ src/components/login.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
+	import { user, auth } from '../stores';
+	import refreshUser from '../lib/refreshUser';
 	const clientID = import.meta.env.VITE_CLIENT_ID;
 	const appURL = import.meta.env.VITE_APP_URL;
 	const vercelURL = import.meta.env.VITE_VERCEL_URL;
-	const redirect = vercelURL || appURL;
-	import { user, auth } from '../stores';
-	import refreshUser from '../lib/refreshUser';
+
+	let redirect = vercelURL;
+	if (!redirect.toString().startsWith('http')) {
+		redirect = `https://${redirect}`;
+	}
+
 	let url = `http://www.strava.com/oauth/authorize?client_id=${clientID}&response_type=code&redirect_uri=${redirect}&approval_prompt=force&scope=read,activity:read_all`;
 
 	async function attemptLogin(event: Event) {
