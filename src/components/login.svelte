70a2317b40b4c50010e7d4769baa5f46edbe0a55
Charles Harries <charles@harries.me>
2022-03-26 14:31:08 +0000

    Update redirect

--- src/components/login.svelte
+++ src/components/login.svelte
@@ -2,13 +2,10 @@
 	const clientID = import.meta.env.VITE_CLIENT_ID;
 	const appURL = import.meta.env.VITE_APP_URL;
 	const vercelURL = import.meta.env.VERCEL_URL;
+	const redirect = vercelURL || appURL;
 	import { user, auth } from '../stores';
 	import refreshUser from '../lib/refreshUser';
-	let url = `http://www.strava.com/oauth/authorize?client_id=${clientID}&response_type=code&redirect_uri=${appURL}&approval_prompt=force&scope=read,activity:read_all`;
-
-	if (vercelURL) {
-		url = `http://${vercelURL}`;
-	}
+	let url = `http://www.strava.com/oauth/authorize?client_id=${clientID}&response_type=code&redirect_uri=${redirect}&approval_prompt=force&scope=read,activity:read_all`;
 
 	async function attemptLogin(event: Event) {
 		if ($user.refresh_token) {
