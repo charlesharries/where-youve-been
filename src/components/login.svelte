7c9c390b26dd1d665ff3d0e7f9cf022168953797
Charles Harries <charles@harries.me>
2022-03-27 18:25:21 +0100

    Use VITE_APP_URL

--- src/components/login.svelte
+++ src/components/login.svelte
@@ -2,14 +2,9 @@
 	import { user, auth } from '../stores';
 	import refreshUser from '../lib/refreshUser';
 	const clientID = import.meta.env.VITE_CLIENT_ID;
-	const vercelURL = import.meta.env.VITE_VERCEL_URL;
+	const redirectURL = import.meta.env.VITE_APP_URL;
 
-	let redirect = vercelURL;
-	if (!redirect.toString().startsWith('http')) {
-		redirect = `https://${redirect}`;
-	}
-
-	let url = `http://www.strava.com/oauth/authorize?client_id=${clientID}&response_type=code&redirect_uri=${redirect}&approval_prompt=force&scope=read,activity:read_all`;
+	let url = `http://www.strava.com/oauth/authorize?client_id=${clientID}&response_type=code&redirect_uri=${redirectURL}&approval_prompt=force&scope=read,activity:read_all`;
 
 	async function attemptLogin(event: Event) {
 		if ($user.refresh_token) {
