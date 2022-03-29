baf24df6d736e2bcdcdbc83dd9b2c672c7a38de8
Charles Harries <charles@harries.me>
2022-03-29 17:21:27 +0100

    Add favicon & page meta

--- src/components/fetchActivities.svelte
+++ src/components/fetchActivities.svelte
@@ -51,7 +51,7 @@
 >
 	<svg
 		xmlns="http://www.w3.org/2000/svg"
-		class="h-md w-md"
+		class="h-sm w-sm"
 		fill="none"
 		viewBox="0 0 24 24"
 		stroke="currentColor"
@@ -63,4 +63,5 @@
 			d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
 		/>
 	</svg>
+	<span>{$loadingState === 'idle' ? 'Fetch activities' : 'Fetching...'}</span>
 </button>
--- src/components/logout.svelte
+++ src/components/logout.svelte
@@ -6,4 +6,4 @@
 	}
 </script>
 
-<button on:click={handleLogout} class="button">Log out</button>
+<button on:click={handleLogout} class="button text-danger">Log out</button>
