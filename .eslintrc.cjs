71d13a941196c034feb433f79a67cc849e252bdf
Charles Harries <charles@harries.me>
2025-07-21 21:06:46 +0100

    Update a whole bunch of dependencies

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,9 +2,9 @@ module.exports = {
 	root: true,
 	parser: '@typescript-eslint/parser',
 	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-	plugins: ['svelte3', '@typescript-eslint'],
+	plugins: ['svelte', '@typescript-eslint'],
 	ignorePatterns: ['*.cjs'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+	overrides: [{ files: ['*.svelte'], parser: "svelte-eslint-parser" }],
 	settings: {
 		'svelte3/typescript': () => require('typescript')
 	},
